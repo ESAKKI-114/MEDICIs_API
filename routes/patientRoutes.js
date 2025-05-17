2493d90d601e56d640ed55bc514e0de0bd7906a5
PAUL ESAKKI <51704403+ESAKKI-114@users.noreply.github.com>
2025-05-17 18:12:16 +0530

    Update patientRoutes.js

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -2,23 +2,24 @@ const express = require('express');
 const router = express.Router();
 const verifyRole = require('../verifyRole');
 const controller = require('../controllers/patientController');
+const verifyToken = require('../controllers/verifyToken');
 
 
-router.get('/exist', controller.checkTokenExistenceByDate);
+router.get('/exist',verifyToken, controller.checkTokenExistenceByDate);
 
 // Route to check token availability
-router.get('/max-token', controller.getMaxTokenIdForToday);
+router.get('/max-token',verifyToken, controller.getMaxTokenIdForToday);
 
 // 🟢 Public or unrestricted access (read-only)
-router.get('/', verifyRole(['ADMIN', 'HOSPITAL','CLINIC']),controller.getAllPatients);
-router.get('/:id',verifyRole(['ADMIN', 'HOSPITAL','CLINIC']), controller.getPatientById);
+router.get('/',verifyToken, verifyRole(['ADMIN', 'HOSPITAL','CLINIC']),controller.getAllPatients);
+router.get('/:id',verifyToken,verifyRole(['ADMIN', 'HOSPITAL','CLINIC']), controller.getPatientById);
 
 // 🟡 Restricted: ADMIN and INTERNAL can create and update
-router.post('/', verifyRole(['ADMIN', 'HOSPITAL','CLINIC']), controller.createPatient);
-router.put('/:id', verifyRole(['ADMIN', 'HOSPITAL','CLINIC']), controller.updatePatient);   
+router.post('/', verifyToken,verifyRole(['ADMIN', 'HOSPITAL','CLINIC']), controller.createPatient);
+router.put('/:id', verifyToken,verifyRole(['ADMIN', 'HOSPITAL','CLINIC']), controller.updatePatient);   
 
 // 🔴 Strict: Only ADMIN can delete
-router.delete('/:id', verifyRole(['ADMIN']), controller.deletePatient);
+router.delete('/:id',verifyToken, verifyRole(['ADMIN']), controller.deletePatient);
 
   
 
